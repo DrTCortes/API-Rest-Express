c2d0454507873147b1d629792ba5a11bfdd5b30e
DrTCortes <thiagocortes@outlook.com.br>
2021-11-12 20:36:26 -0300

    Criando rota Post

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,23 @@
 const userList = require('./database');
 
+let nextId = userList.length +1;
+let professoesValidas = ["logica", "ui/ux", "front-end", "back-end", "full-stack", "mobile", "soft-skills"];
+
+function validateUser(user){
+    console.log(user.profissao)
+    if(!user.nome || typeof user.nome !== "string"){
+        return'O campo "nome" deve ser preenchido corretamente';
+    }
+
+    if(!user.idade || typeof user.idade != "number" || user.idade < 18){
+        return'O campo "idade" deve ser preenchido corretamente';
+    }
+
+    if(!user.profissao || typeof user.profissao != "string" || !professoesValidas.includes(user.profissao.toLowerCase())){
+        return'O campo "profissão" deve ser preenchido corretamente';
+    } 
+}
+
 function allQuery(req, res){
     res.json(userList);
 }
@@ -18,6 +36,26 @@ function singleQuery(req, res){
     res.json(user);
 }
 
+function createData(req, res){
+    const error = validateUser(req.body)
 
+    if(error){
+        res.status(400);
+        res.json(error);
+        return;
+    }
 
-module.exports = {allQuery, singleQuery}
+    const newUser = {
+        id: nextId,
+        nome: req.body.nome,
+        idade: req.body.idade,
+        profissao: req.body.profissao
+    };
+
+    userList.push(newUser);
+    nextId += 1;
+
+    res.json(userList);
+}
+
+module.exports = {allQuery, singleQuery, createData}
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express()
-const {allQuery, singleQuery} = require('./controller')
+const {allQuery, singleQuery, createData} = require('./controller')
 
 router.get("/users", allQuery);
 
 router.get("/users/:idQuery", singleQuery);
+
+router.post("/users", createData);
 
 module.exports = router
